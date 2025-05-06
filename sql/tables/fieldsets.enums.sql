0f1d812f762149301d88d22a45cc814c331aa5e7
willgladstone <will.gl@dst.one>
2025-05-05 23:49:21 -0400

    fix enums and records

--- sql/tables/fieldsets.enums.sql
+++ sql/tables/fieldsets.enums.sql
@@ -2,15 +2,18 @@
  * Enumerated Fields
  * Uses native low-cardinality data type
  */
-CREATE TABLE IF NOT EXISTS fieldsets.enums (
+CREATE DICTIONARY IF NOT EXISTS fieldsets.enums (
     id          UInt64, -- References fieldset id
-    token       LowCardinality(String), -- References fieldset token
+    token       String, -- References fieldset token
     field_id    UInt64,
-    field_token LowCardinality(String),
-    created     DateTime64(3) DEFAULT now64(3)
+    field_token String,
+    created     DateTime64(3)
 )
-ENGINE = ReplacingMergeTree(created)
-PARTITION BY field_id
-PRIMARY KEY (field_id, id)
-ORDER BY (field_id, id)
-SETTINGS clean_deleted_rows='Always';
+PRIMARY KEY field_token, token
+SOURCE(PostgreSQL(
+    NAME 'postgres_connection'
+    TABLE 'enums'
+    DB 'fieldsets'
+))
+LAYOUT(HASHED())
+LIFETIME (MIN 1 MAX 2);
